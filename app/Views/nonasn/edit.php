7e11c01de44371173fb131a1333d8ceaf8b62935
danuri <danualbantani@gmail.com>
2024-01-16 13:25:19 +0700

    Update edit.php

--- app/Views/nonasn/edit.php
+++ app/Views/nonasn/edit.php
@@ -124,7 +124,7 @@
                   <div class="col-lg-9">
                     <select class="form-select select2" name="jabatan" id="jabatan">
                       <?php foreach ($jabatan as $row) {
-                        $select = ($row->nama == $nonasn->pendidikan_baru)?'selected':'';
+                        $select = ($row->nama_jabatan == $nonasn->pendidikan_baru)?'selected':'';
                         echo '<option value="'.$row->nama_jabatan.'" '.$select.'>'.$row->nama_jabatan.' ('.$row->group.')</option>';
                       } ?>
                     </select>
