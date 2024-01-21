52cee666eb9f9bfd6bf1c50464cfc72b7652d1e5
danuri <danualbantani@gmail.com>
2024-01-21 13:15:00 +0700

    Update CrudModel.php

--- app/Models/CrudModel.php
+++ app/Models/CrudModel.php
@@ -79,7 +79,15 @@ class CrudModel extends Model
       public function getRekapPppk()
       {
         $kodesatker = session('kodesatker');
-        $query = $this->db->query("SELECT jabatan_baru,unit_penempatan_nama_baru, COUNT(*) AS jumlah FROM nonasn WHERE status_pegawai='NON ASN' AND status_pemetaan='Aktif' AND jabatan_baru IS NOT NULL AND KODE_SATKER_PARENT='$kodesatker'
+        $query = $this->db->query("SELECT jabatan_baru,unit_penempatan_nama_baru, COUNT(*) AS jumlah FROM nonasn WHERE status_pegawai='NON ASN' AND status_nonasn='Aktif' AND jabatan_baru IS NOT NULL AND KODE_SATKER_PARENT='$kodesatker'
+                                    GROUP BY jabatan_baru, unit_penempatan_nama_baru")->getResult();
+        return $query;
+      }
+
+      public function getRekapCpns()
+      {
+        $kodesatker = session('kodesatker');
+        $query = $this->db->query("SELECT jabatan_baru,unit_penempatan_nama_baru, COUNT(*) AS jumlah FROM nonasn WHERE status_pegawai='NON ASN' AND status_nonasn='Aktif' AND jabatan_baru IS NOT NULL AND KODE_SATKER_PARENT='$kodesatker'
                                     GROUP BY jabatan_baru, unit_penempatan_nama_baru")->getResult();
         return $query;
       }
